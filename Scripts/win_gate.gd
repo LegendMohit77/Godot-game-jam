c54102268ae2bb1215f8d41214a235de8d6d4abd
YONOICE <theshreyanshsingh@gmail.com>
2025-03-22 16:55:27 +0530

    basic level done
    spike and win gate done ache se

--- Scripts/win_gate.gd
+++ Scripts/win_gate.gd
@@ -2,8 +2,15 @@ extends Area2D
 @export var next_level: String ="res://Scenes/testlevel.tscn"
 
 
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 	
 
 func _on_body_entered(body:CharacterBody2D) -> void:
+	
+		animated_sprite.play("Open")
+		
+		await animated_sprite.animation_finished
+		get_tree().paused=true
+		await get_tree().create_timer(0.5).timeout
+		get_tree().paused=false
 		get_tree().change_scene_to_file(next_level)
