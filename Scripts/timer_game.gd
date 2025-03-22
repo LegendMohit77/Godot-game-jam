1ede2193322bc6d6f502cdb569f6ec13c364e030
YONOICE <theshreyanshsingh@gmail.com>
2025-03-22 23:04:45 +0530

    smol chnages in timer code

--- Scripts/timer_game.gd
+++ Scripts/timer_game.gd
@@ -7,4 +7,7 @@ func _process(delta):
 	timer_label.text = "Time: " + str(ceil(timer.time_left))  # Update UI
 
 func _on_timer_timeout() -> void:
-	get_tree().reload_current_scene()  # Reload scene when time runs out
+		get_tree().paused=true
+		await get_tree().create_timer(0.5).timeout
+		get_tree().paused=false
+		get_tree().reload_current_scene()  # Reload scene when time runs out
