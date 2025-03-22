206b86b7b906374cc5618d767378e6caf97f4a4b
LegendMohit77 <mohitmethani77@gmail.com>
2025-03-22 23:20:31 +0530

    timer fix

--- Scripts/timer_game.gd
+++ Scripts/timer_game.gd
@@ -4,10 +4,10 @@ extends Node
 @onready var timer_label := $Label
 
 func _process(delta):
-	timer_label.text = "Time: " + str(ceil(timer.time_left))  # Update UI
+	timer_label.text = "Time: " + str(ceil(timer.time_left)) 
 
 func _on_timer_timeout() -> void:
 		get_tree().paused=true
 		await get_tree().create_timer(0.5).timeout
 		get_tree().paused=false
-		get_tree().reload_current_scene()  # Reload scene when time runs out
+		get_tree().change_scene_to_file("res://Scenes/retry_screen.tscn")
